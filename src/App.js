6ab8bb753dc7a3e091b4aeef72f104fb4e8dc291
murat <murat.tc2014@gmail.com>
2023-03-16 10:29:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Images from "./Images";
+import "./styles.css";
 
 function App() {
+  const [first, setFirst] = useState(Images[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src={first} alt="selected" className="selected" />
+      </div>
+      <div className="imgContainer">
+        {Images.map((img, index) => (
+          <img
+            src={img}
+            key={index}
+            alt="tan"
+            onClick={()=>setFirst(img)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Images.jsx
@@ -0,0 +1,12 @@
+
+const Images =  [
+  
+        "https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2016/11/22/23/23/french-fries-1851143_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/02/15/10/57/pizza-2068272_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/03/31/18/02/strawberry-dessert-2191973_960_720.jpg"
+  
+]
+
+export default Images
